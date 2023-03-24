fb34a877b73f8140f1b52372d04e7a68d3c80144
TimoshaPonchik <nightfli@mail.ru>
2023-03-24 12:26:11 +0300

    PolyTask2 main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import java.io.*;
+
+
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
 
 public class Main {
@@ -23,51 +25,89 @@ public class Main {
 
     @Argument
     private List<String> arguments = new ArrayList<String>();
-    private Boolean checker = true;
 
-    public static void main(String[] args) throws CmdLineException {
+    public static void main(String[] args) throws CmdLineException, IOException {
         new Main().parser(args);
     }
 
-    private void parser(String[] args) throws CmdLineException {
+    private void parser(String[] args) throws CmdLineException, IOException {
         CmdLineParser parser = new CmdLineParser(this);
         parser.parseArgument(args);
-
-        try {
-            FileReader a = new FileReader(arguments.get(arguments.size() - 1));
-        } catch (Exception e) {
-            checker = false;
-        }
-        algorithm(arguments);
+        algorithm();
     }
 
-    public void algorithm(List<String> list) {
-        Scanner abc = new Scanner(System.in);
+    public void algorithm() throws IOException {
         int colivo = 0;
         String temp = "";
-        boolean a = true;
-        List<String> lines = new ArrayList<>();
         String line = "";
-        while (!Objects.equals(line, "`")) {
-            line = abc.nextLine();
-            if (a || temp.equals(line)) {
-                a = false;
-                colivo++;
-                temp = line;
-            } else {
-                if (colivo > 1) {
-                    lines.add(colivo + temp);
+        List<String> lines = new ArrayList<>();
+        while (!Objects.equals(line, "`") && line != null) {
+            if (arguments.isEmpty()) {
+                Scanner abc = new Scanner(System.in);
+                if (register) {
+                    line = abc.nextLine().toLowerCase();
                 } else {
+                    line = abc.nextLine();
+                }
+            } else {
+                File file = new File(arguments.get(0));
+                FileReader fr = new FileReader(file);
+                BufferedReader reader = new BufferedReader(fr);
+                if (register) {
+                    try {
+                        line = reader.readLine().toLowerCase();
+                    } catch (Exception e) {
+                        line = reader.readLine();
+                    }
+                } else {
+                    line = reader.readLine();
+                }
+            }
+            if (num > 0) {
+                line = removefirstChar(line, num);
+            }
+            if (temp.equals(line) || colivo == 0) {
+                colivo++;
+            } else {
+                if (colivo > 1 && !unique) {
+                    if (change) {
+                        lines.add(colivo + temp);
+                    } else {
+                        lines.add(temp);
+                    }
+                } else if (colivo == 1) {
                     lines.add(temp);
                 }
-                temp = line;
                 colivo = 1;
             }
+            temp = line;
         }
-        System.out.println(lines);
+        inputer(lines);
     }
 
-    public void doMethod(Scanner line) {
+    public String removefirstChar(String str, Integer num) {
+        if (str == null || str.length() == 0 || str.equals("`")) {
+            return str;
+        }
+        if (str.length() > num) {
+            return str.substring(num);
+        } else {
+            return "";
+        }
+    }
 
+    public void inputer(List<String> list) throws IOException {
+        if (ofile == null) {
+            for (String line : list) {
+                System.out.println(line);
+            }
+        } else {
+            FileWriter writer = new FileWriter(ofile, false);
+            for (String line : list) {
+                writer.write(line);
+                writer.write("\n");
+                writer.flush();
+            }
+        }
     }
 }
